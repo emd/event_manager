b3610de570a8bf68a0a66a08e7a6f005950f2d9c
emd <emd@mit.edu>
2015-04-03 17:27:40 -0700

    Including standard project dirs/files

---
+++ setup.py
@@ -0,0 +1,17 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'name': 'event_manager',
+    'version': '0.1',
+    'packages': ['event_manager'],
+    'install_requires': ['sys', 'numpy', 'matplotlib', 'nose'],
+    'author': 'Evan M. Davis',
+    'author_email': 'emd@mit.edu',
+    'url': '',
+    'description': 'Event management routines for Python.'
+}
+
+setup(**config)
